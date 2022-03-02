2c68aae87c9a633e03dfb3aba3974f11695ea8a7
lxf <leon.rosenfeild@gmail.com>
2022-03-01 19:47:21 -0500

    added

---
+++ ads1115/ads1115.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+Adafruit_ADS1X15 ads;
+float Voltage = 0.0;
+
+void setup(void) 
+{
+Serial.begin(115200); 
+ads.begin();
+}
+
+void loop(void) 
+{
+int16_t adc0;
+
+adc0 = ads.readADC_SingleEnded(0);
+float volts0 = ads.computeVolts(adc0);
+Voltage = (adc0 * 0.1875)/1000;
+
+Serial.print("AIN0: "); 
+Serial.print(adc0);
+Serial.print("\tVoltage: ");
+Serial.print(Voltage, 7);
+Serial.print("\tVoltage func, "); 
+Serial.println(volts0,7); 
+Serial.println();
+
+delay(1000);
+}
